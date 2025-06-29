58ca1be930045a1308aee63ab8d6f258edd0ff3b
resister-boy <pesticide.dev@gmail.com>
2025-06-30 00:03:14 +0900

    example: update package example

--- example/main.ts
+++ example/main.ts
@@ -1,11 +1,9 @@
-import { MemetusPumpfun } from "@0xobedient/memetus-pumpfun-sdk";
+import { MemetusPumpfun, TokenResponse } from "@0xobedient/memetus-pumpfun-sdk";
 
 const main = async () => {
   const memetusClient = new MemetusPumpfun();
 
-  const res = await memetusClient.fetchTokenByAddress(
-    "7h47P3b173g1nRrtqYNRY9nYwuVcAbM9rtB3DXNypump"
-  );
+  const res = (await memetusClient.fetchRecentTokens({})) as TokenResponse[];
 
   console.log(res);
 };
